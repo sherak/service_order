fa77dcb80931bba100eb52cf6f44ddff4400f1fb
sherak <herak4@gmail.com>
2017-08-02 10:13:26 +0200

    Refactoring search engine

--- search_engine.php
+++ search_engine.php
@@ -48,10 +48,10 @@ $c = new db_connection();
 $sql = "SELECT category, type FROM occupation";
 $data = $c->query($sql);
 foreach ($data as $key => $value) {
-	array_push($a_json, $value['category']);
-	array_push($a_json, $value['type']);	
+	$a_json[] =  $value['category'];
+	$a_json[] = $value['type'];	
 }
-$a_json = array_unique($a_json);
+$a_json = array_values(array_unique($a_json));
 $sql = "SELECT * FROM occupation INNER JOIN service_provider ON occupation.occupation_id = service_provider.fk_occupation_id INNER JOIN user ON user.user_id = service_provider.fk_user_id WHERE (category LIKE '%$term%' OR type LIKE '%$term%') AND (city = '$city') ORDER BY category, type" or die("Failed to query database" . mysql_error());
 // TODO: add implementation for quart
 if($data = $c->query($sql)) {
@@ -115,47 +115,6 @@ $str = $x->start_tag($tag, $attr_ar);
 $str .= 'Back';
 $str .= $x->end_tag($tag);
 echo $str;
-	
-/*$fk_user_ids = array();
-$user_ids = array();	
-// TODO: better solution - query in for loop 											
-foreach($occ_data as $key => $value) {
-	print_r($value);
-	$sql = "SELECT fk_user_id FROM service_provider WHERE fk_occupation_id = " . $value['occupation_id'] . "" or die("Failed to query database" . mysql_error());
-	array_push($fk_user_ids, $c->query($sql)[0]);
-	echo '<br>';
-}
-foreach($sp_data as $key => $value) {
-	print_r($value);
-	$sql = "SELECT user_id FROM user WHERE user_id = " . $value['fk_user_id'] or die("Failed to query database" . mysql_error());
-	array_push($user_ids, $c->query($sql)[0]);
-	echo '<br>';
-}
-$tmp1 = array_merge($fk_user_ids, $user_ids);
-$searched_user_ids = array();
-foreach($tmp1 as $key => $value) {
-	array_push($searched_user_ids, $value[key($value)]);
-}
-$searched_sp_ids = array_unique($searched_user_ids);
-$service_providers = array;
-foreach ($searched_sp_ids as $key => $value) {
-	
-}*/
-/*if($occ_data = $conn->query($sql1) and $sp_data = $conn->query($sql2)) {
-	while($row = mysqli_fetch_array($occ_data)) {
-		$category = htmlentities(stripslashes($row['category']));
-		$type = htmlentities(stripslashes($row['type']));
-		$a_json_row["value"] = $category.' '.$type;
-		$a_json_row["label"] = $category.' '.$type;
-		array_push($a_json, $a_json_row);
-	}
-	while($row = mysqli_fetch_array($sp_data)) {
-		$city = htmlentities(stripslashes($row['city']));
-		$quart = htmlentities(stripslashes($row['city']));
-		$a_json_row["value"] = $city.' '.$quart;
-		$a_json_row["label"] = $city.' '.$quart;
-		array_push($a_json, $a_json_row);
-	}
-}*/
+
 echo json_encode($a_json);
 flush();
